4d57d52831f17e767cbfdb207a0fd1448b30cbf3
lucas93298 <lucas.fernandez93@hotmail.com>
2020-09-28 22:12:18 -0300

    Updates

---
+++ precache-manifest.aff71a93adb2432d43b13ad9979a58b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb324d14d4ba800642f8ed882afcd69",
+    "url": "/SeleccionDeDiscos/index.html"
+  },
+  {
+    "revision": "424dbf374ace5780570b",
+    "url": "/SeleccionDeDiscos/static/js/2.63b95a2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SeleccionDeDiscos/static/js/2.63b95a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8453ec6552beaa7769",
+    "url": "/SeleccionDeDiscos/static/js/main.a684dc88.chunk.js"
+  },
+  {
+    "revision": "d4b2c5fd66e899f68a83",
+    "url": "/SeleccionDeDiscos/static/js/runtime-main.eab225f9.js"
+  },
+  {
+    "revision": "7c266a8bc7c59b7b9733f2a55507d48d",
+    "url": "/SeleccionDeDiscos/static/media/cancha.7c266a8b.jpeg"
+  }
+]);
